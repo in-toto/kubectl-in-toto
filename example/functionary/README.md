09930ced0ab0c6cd74e2e7c0935ea53c9597f584
Santiago Torres <santiago@archlinux.org>
2019-04-17 16:13:51 -0400

    example: add example directory

---
+++ example/functionary/README.md
@@ -0,0 +1,22 @@
+Functionary step
+================
+
+This is somewhat a silly example to produce a small image that can be kept in
+the cluster running without actually doing anything (I'm sure there are ways to
+do this without a 700k binary, but oh well). First, you need to compile the
+entrypoint:
+
+```
+gcc main.c
+```
+
+Then build the image and create an in-toto link. To do this easily in minikube
+I suggest exposing the docker socket:
+
+```
+eval $(minikube docker-env)
+in-toto-run -k functionary -n build -p image_id -- docker build . -t empty:1.0.0 --iidfile image_id
+```
+
+as a side note, the `image_id` file will be used to serialize the hash of the
+resulting image to a file to be tracked as an in-toto product.
---
+++ example/project-owner/create_layout.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import in_toto.models.layout as l
+import in_toto.models.metadata as m
+import in_toto.util as util
+import shlex
+
+# keys = util.import_gpg_public_keys_from_keyring_as_dict(['functionary.pub'])
+project_owner = util.import_rsa_key_from_file('project-owner')
+
+layout = l.Layout()
+keyid = layout.add_functionary_key_from_path('functionary.pub')['keyid']
+
+step = l.Step()
+step.name = 'build'
+step.materials = []
+step.products = ['CREATE', 'image_id']
+step.expected_command = shlex.split('docker build . -t empty --idfile image_id')
+step.pubkeys = [keyid]
+
+inspection = l.Inspection()
+inspection.name = 'verify-image'
+inspection.expected_materials = [['MATCH', 'image_id', 'WITH', 'PRODUCTS', 'FROM', 'build']]
+inspection.expected_products = []
+inspection.run = shlex.split("echo", "lol")
+
+layout.steps.append(step)
+layout.inspect.append(inspection)
+
+metablock = m.Metablock(signed=layout)
+metablock.sign(project_owner)
+metablock.dump("root.layout")
